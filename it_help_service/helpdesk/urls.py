cc248735e15f63e00b6a180e18442fdc59d1a6b6
Artem Borovik <borovikao@mail.ru>
2022-07-07 22:21:54 +0600

    update helpdesk.urls, helpdesk.views

--- it_help_service/helpdesk/urls.py
+++ it_help_service/helpdesk/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /helpdesk/
     path('', views.index, name='index'),
+    # /helpdesk/orders
+    path('orders/', views.orders, name='orders'),
+    # /helpdesk/orders/1
+    path('orders/<int:order_id>/', views.detail, name='detail'),
 ]
--- it_help_service/helpdesk/views.py
+++ it_help_service/helpdesk/views.py
@@ -2,4 +2,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the helpdesk index.")
+    # todo должна быть логика для новых отображения обращений (Админ видит все обращения, а обычный пользователь только свои)
+    # todo добавить шаблон главной страницы
+    return HttpResponse("Здесь будет отображаться список новых обращений")
+
+
+def detail(request, order_id):
+    # todo добавить шаблон для отображения деталей по заказу
+    return HttpResponse(f"Детализация по задаче №{order_id}")
+
+
+def orders(request):
+    # todo добавить шаблон для отображения списка всех обращений
+    return HttpResponse("Здесь будет отображаться список всех обращений")
